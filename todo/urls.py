02288755a1593c542a009a7f9f740037e6533873
unknown <matheus.melo@centralit.com.br>
2024-04-08 17:26:37 -0300

    Adicionando logica para edição de tarefas existentes.

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Add new task
     path("addTask/", views.addTask, name='addTask'),
+
+    # Mark task as done
     path("mark_as_done/<int:pk>/", views.mark_as_done, name='mark_as_done'),
+
+    # Mark task as undone
     path("mark_as_undone/<int:pk>/", views.mark_as_undone, name='mark_as_undone'),
+
+    # Edit selected task
+    path("edit_task/<int:pk>/", views.edit_task, name='edit_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Task
 
@@ -18,3 +18,16 @@ def mark_as_undone(request, pk):
     task.is_completed = False
     task.save()
     return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+    return render(request, 'edit_task.html', context)
